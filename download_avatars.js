60e34ecf196241f33c745fff12a8a00a35fc860a
anotherwan <itsanotherwan@gmail.com>
2017-01-16 17:28:59 -0500

    started getRepoContributors function and invoked it by hard-coding params

--- download_avatars.js
+++ download_avatars.js
@@ -2,4 +2,19 @@
 
 const request = require('request')
 
-console.log('Welcome to the Github Avatar Downloader!')
+function getRepoContributors(repoOwner, repoName, cb) {
+  request("https://api.github.com/repos/jquery/jquery/contributors")
+    .on('error', function (err) {
+      throw err
+    })
+    .on('response', function (response) {
+      console.log('Downloading...')
+      console.log('Response Status Code:', response.statusMessage, 'Content Type:', response.headers['content-type'])
+    })
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, results) {
+  console.log("Errors:", err)
+  console.log("Result:", result)
+})
